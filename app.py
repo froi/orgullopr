288cd02ea49e60a62ddb7ed95e8db4094d96e1c5
Froilan Irizarry <irizarry.froilan@gmail.com>
2014-03-04 03:58:52 -0400

    Added input form. Added input form CSS
    Eliminated app css from main.css

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.secret_key="a"
 
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'orgullopr.db'),
@@ -14,6 +15,9 @@ app.config.from_envvar('ORGULLOPR_SETTINGS', silent=True)
 
 
 def dict_factory(cursor, row):
+    """
+    Used to substitute the sqlite.Row factory. This is done to avoid the tuple output from sqlite.Row.
+    """
     d = {}
     for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
@@ -23,13 +27,16 @@ def dict_factory(cursor, row):
 def connect_db():
     """Connects to the specific database."""
     rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = dict_factory #sqlite3.Row
+    rv.row_factory = dict_factory
     return rv
 
 
 # TODO: set this up in a seperate setup.py file or something
 def init_db():
-    """Initializes the application database"""
+    """
+    Initializes the application database
+    """
+
     with app.app_context():
         db = get_db()
 
@@ -45,7 +52,10 @@ def init_db():
 
 
 def get_db():
-    """Opens a new database connection if there is none yet for the current application context."""
+    """
+    Opens a new database connection if there is none yet for the current application context.
+    """
+
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = connect_db()
     return g.sqlite_db
@@ -61,21 +71,28 @@ def run_query(query, bindings=None):
 
 
 def load_towns():
-    """Loads into session all towns (municipios) in PR """
-    if "municipios" not in session:
-        session['municipios'] = run_query('select id, name from municipios')
+    """
+    Loads into session all towns (municipios) in PR
+    """
+    if not hasattr(g, 'towns'):
+        g.towns = run_query('select id, name from municipios')
+    return g.towns
 
 
 @app.teardown_appcontext
 def close_db(error):
-    """Closes the database again at the end of the request"""
+    """
+    Closes the database again at the end of the request
+    """
+
     if hasattr(g, "sqlite_db"):
         g.sqlite_db.close
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    load_towns()
+    return render_template('index.html', towns=g.towns)
 
 
 @app.route('/videos', methods=['GET'])
@@ -85,22 +102,30 @@ def show_videos():
 
 @app.route('/videos/<municipio>', methods=['GET'])
 def get_videos(municipio):
+    """
+    Get all videos from specified city and renders them in the videos view.
+    """
+
     db = get_db()
     # cur = db.execute('select name, youtube_link from testimonials where town=?', [municipio])
     entries = run_query('select name, youtube_link from testimonials where town=?', [municipio])
 
-    return render_template('videos.html', videos=entries, municipio=municipio)
-
+    if entries and len(entries):
+        return render_template('videos.html', videos=entries, municipio=municipio)
+    else:
+        flash('No se encontraron videos.')
+        return render_template('videos.html', municipio=municipio)
 
 @app.route('/add', methods=['POST'])
 def add_entry():
     db = get_db()
-    db.execute('INSERT INTO entries (name, town, proud_of, pride_in, youtube_link) VALUES(?,?,?,?,?)',
-               [request.form['name'], request.form['town'], request.form['proud_of'], request.form['pride_in'],
-                request.form['youtube_link']])
-    db.commit()
+    print request.form
+    # db.execute('INSERT INTO entries (name, town, proud_of, pride_in, youtube_link) VALUES(?,?,?,?,?)',
+    #            [request.form['name'], request.form['prof'], request.form['town'], request.form['proudof'],
+    #             request.form['pridein'], request.form['pridegov'], request.form['youtubelink']])
+    # db.commit()
     flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
